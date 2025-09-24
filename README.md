183cfbf41ce98259a4c7595067eadf4db5fd3f0b
goncaloo <craftinatorssx@gmail.com>
2025-09-24 12:02:06 -0400

    feat: major changes

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# fock
-A minimal quantum computational library for fermionic systems
-
-I'll get to this I promise
+TODO
--- include/fock/multiple_particle.h
+++
@@ -1,9 +0,0 @@
-#ifndef MULTIPLE_PARTICLE_H
-#define MULTIPLE_PARTICLE_H
-
-namespace fock
-{
-
-}
-
-#endif //MULTIPLE_PARTICLE_H
