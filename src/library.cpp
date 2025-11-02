06a5fe6d165d81448367b855eb15b1b6c276b71d
goncaloo <craftinatorssx@gmail.com>
2025-11-01 23:21:01 -0400

    feat: add `is_valid_region_composition`

--- src/library.cpp
+++ src/library.cpp
@@ -75,3 +75,9 @@ std::vector<unsigned long long> generate_hilbert_subspace(const unsigned int num
     }
     return states;
 }
+
+bool is_valid_region_composition(const unsigned int num_filled_sites,
+    const unsigned long long subregion_state_bitmask, const unsigned long long complement_subregion_state_bitmask)
+{
+    return __builtin_popcountll(subregion_state_bitmask) + __builtin_popcountll(complement_subregion_state_bitmask) == num_filled_sites;
+}
