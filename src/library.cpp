56bdc67d832c468b84e1c2e0f09c060e9fcef86f
goncaloo <craftinatorssx@gmail.com>
2025-11-04 16:05:07 -0500

    feat: add `get_composite_state_bitmask` and fix platform dependent bugs

--- src/library.cpp
+++ src/library.cpp
@@ -2,14 +2,14 @@
 #include <numeric>
 #include <stdexcept>
 
-inline unsigned long long get_next_bit_permutation(const unsigned long long current_bitmask)
+inline std::uint64_t get_next_bit_permutation(const std::uint64_t current_bitmask)
 {
-    const unsigned long long least_significant_bit = current_bitmask & -current_bitmask;
-    const unsigned long long next_rippled_bitmask = current_bitmask + least_significant_bit;
+    const std::uint64_t least_significant_bit = current_bitmask & -current_bitmask;
+    const std::uint64_t next_rippled_bitmask = current_bitmask + least_significant_bit;
     return ((next_rippled_bitmask ^ current_bitmask) >> 2) / least_significant_bit | next_rippled_bitmask;
 }
 
-unsigned long long get_binomial_coefficient(const unsigned int n, unsigned int k)
+std::uint64_t get_binomial_coefficient(const std::uint32_t n, std::uint32_t k)
 {
     if (k > n)
     {
@@ -23,32 +23,32 @@ unsigned long long get_binomial_coefficient(const unsigned int n, unsigned int k
 
     if (k == 0)
     {
-        return 1;
+        return 1ULL;
     }
 
-    unsigned long long result = 1;
-    for (unsigned int i = 0; i < k; ++i)
+    std::uint64_t result = 1ULL;
+    for (std::uint32_t i = 0; i < k; ++i)
     {
-        result = result * (n - i) / (i + 1);
+        result = result * (n - i) / (i + 1ULL);
     }
     return result;
 }
 
-std::vector<unsigned long long> generate_full_hilbert_space(const unsigned int num_sites)
+std::vector<std::uint64_t> generate_full_hilbert_space(const std::uint32_t num_sites)
 {
     if (num_sites >= 64)
     {
-        throw std::runtime_error("Number of sites must be less than 64 to fit in unsigned long long and std::vector.");
+        throw std::runtime_error("Number of sites must be less than 64 to fit in std::uint64_t and std::vector.");
     }
 
     const std::size_t num_states = 1ULL << num_sites;
-    std::vector<unsigned long long> states(num_states);
+    std::vector<std::uint64_t> states(num_states);
     std::iota(states.begin(), states.end(), 0ULL);
     return states;
 }
 
-std::vector<unsigned long long> generate_hilbert_subspace(const unsigned int num_sites, const unsigned int num_filled_sites,
-                                                          unsigned long long &num_states)
+std::vector<std::uint64_t> generate_hilbert_subspace(const std::uint32_t num_sites, const std::uint32_t num_filled_sites,
+                                                     std::uint64_t &num_states)
 {
     if (num_filled_sites > num_sites)
     {
@@ -56,20 +56,20 @@ std::vector<unsigned long long> generate_hilbert_subspace(const unsigned int num
     }
 
     num_states = get_binomial_coefficient(num_sites, num_filled_sites);
-    std::vector<unsigned long long> states(num_states);
+    std::vector<std::uint64_t> states(num_states);
 
     if (num_filled_sites == 0)
     {
-        if (num_states == 1) states[0] = 0ULL;
+        if (num_states == 1ULL) states[0] = 0ULL;
         return states;
     }
 
-    unsigned long long current_state_bitmask = (1ULL << num_filled_sites) - 1ULL;
+    std::uint64_t current_state_bitmask = (1ULL << num_filled_sites) - 1ULL;
     for (std::size_t state_index = 0; state_index < num_states; ++state_index)
     {
         states[state_index] = current_state_bitmask;
 
-        if (state_index < num_states - 1)
+        if (state_index < num_states - 1ULL)
         {
             current_state_bitmask = get_next_bit_permutation(current_state_bitmask);
         }
@@ -77,23 +77,56 @@ std::vector<unsigned long long> generate_hilbert_subspace(const unsigned int num
     return states;
 }
 
-bool is_valid_region_composition(const unsigned int num_filled_sites,
-                                 const unsigned long long subregion_state_bitmask, const unsigned long long complement_subregion_state_bitmask)
+bool is_valid_region_composition(const std::uint32_t num_filled_sites,
+                                 const std::uint64_t subregion_state_bitmask, const std::uint64_t complement_subregion_state_bitmask)
 {
     return __builtin_popcountll(subregion_state_bitmask) + __builtin_popcountll(complement_subregion_state_bitmask) == num_filled_sites;
 }
 
-bool are_neighbors(const unsigned long long initial_state_bitmask, const unsigned long long final_state_bitmask)
+bool are_neighbors(const std::uint64_t initial_state_bitmask, const std::uint64_t final_state_bitmask)
 {
-    const unsigned long long differing_bitmask = initial_state_bitmask ^ final_state_bitmask;
+    const std::uint64_t differing_bitmask = initial_state_bitmask ^ final_state_bitmask;
     // __builtin_popcountll(differing_bitmask) == 2 ensures that the states differ by exactly 2 bits
     // and differing_bitmask & differing_bitmask >> 1ULL ensures that these differing bits are adjacent.
     return __builtin_popcountll(differing_bitmask) == 2 && differing_bitmask & differing_bitmask >> 1ULL;
 }
 
-// TODO separate this from code above
+inline std::uint64_t pdep_implementation(std::uint64_t source, std::uint64_t mask)
+{
+    std::uint64_t destination = 0;
+    while (source && mask)
+    {
+        const unsigned num_trailing_zeros = __builtin_ctzll(mask);
+        if (source & 1ULL)
+        {
+            destination |= 1ULL << num_trailing_zeros;
+        }
+        source >>= 1ULL;
+        mask &= mask - 1ULL;
+    }
+    return destination;
+}
 
-#define EIGEN_DONT_VECTORIZE // Should I do this?
+std::uint64_t get_composite_state_bitmask(const std::vector<std::uint32_t> &subregion_indices, const int num_sites,
+                                          const std::uint64_t subregion_state_bitmask, const std::uint64_t complement_subregion_state_bitmask)
+{
+    std::uint64_t subregion_mask = 0;
+    for (const std::uint32_t x: subregion_indices)
+    {
+        subregion_mask |= 1ULL << (num_sites - x - 1ULL);
+    }
+    const uint64_t complement_subregion_mask = (1ULL << num_sites) - 1ULL ^ subregion_mask;
+#if defined(__BMI2__) && (defined(__x86_64__) || defined(_M_X64))
+    return _pdep_u64(subregion_state_bitmask, subregion_mask) |
+           _pdep_u64(complement_subregion_state_bitmask, complement_subregion_mask);
+#else
+    return pdep_implementation(subregion_state_bitmask, subregion_mask) |
+           pdep_implementation(complement_subregion_state_bitmask, complement_subregion_mask);
+#endif
+}
+
+// TODO Separate this from code above
+// TODO Check if this is needed for precision #define EIGEN_DONT_VECTORIZE
 #include <Eigen/Dense>
 #include <boost/multiprecision/cpp_dec_float.hpp>
 
@@ -101,9 +134,9 @@ const boost::multiprecision::cpp_dec_float_50 zero("0");
 const boost::multiprecision::cpp_dec_float_50 minus_one("-1");
 
 Eigen::Matrix<boost::multiprecision::cpp_dec_float_50, Eigen::Dynamic, Eigen::Dynamic>
-build_hamiltonian(const unsigned int num_sites, const unsigned int num_filled_sites)
+build_hamiltonian(const std::uint32_t num_sites, const std::uint32_t num_filled_sites)
 {
-    unsigned long long num_states;
+    std::uint64_t num_states;
     const auto states = generate_hilbert_subspace(num_sites, num_filled_sites, num_states);
 
     Eigen::Matrix<boost::multiprecision::cpp_dec_float_50, Eigen::Dynamic, Eigen::Dynamic>
@@ -112,7 +145,7 @@ build_hamiltonian(const unsigned int num_sites, const unsigned int num_filled_si
 
     for (Eigen::Index final_state_index = 0; final_state_index < num_states; ++final_state_index)
     {
-        for (Eigen::Index initial_state_index = final_state_index + 1; initial_state_index < num_states; ++initial_state_index)
+        for (Eigen::Index initial_state_index = final_state_index + 1L; initial_state_index < num_states; ++initial_state_index)
         {
             if (are_neighbors(states[final_state_index], states[initial_state_index]))
             {
