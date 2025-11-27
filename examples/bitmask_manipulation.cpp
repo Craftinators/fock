f89e66aaffd0a73a00b2af1bd5550cc72c290ae2
craftinators <craftinatorssx@gmail.com>
2025-11-27 06:06:24 -0500

    feat(generate_rdm): implement `generate_rdm` and add `composite_state_bitmask` Add `composite_state_bitmask(...)` helper to merge a subregion bitmask and its complement into a full-system bitmask. Update example to solve the Hamiltonian with `Eigen::SelfAdjointEigenSolver`, select the ground-state eigenvector, print it, and compute / print the subregion RDM for `{0, 1}`.

--- examples/bitmask_manipulation.cpp
+++ examples/bitmask_manipulation.cpp
@@ -6,8 +6,16 @@ int main()
 {
     constexpr std::uint32_t num_sites = 4;
     constexpr std::uint32_t num_filled_sites = 2;
-    Eigen::MatrixXd hamiltonian;
 
+    Eigen::MatrixXd hamiltonian;
     fock::generate_hamiltonian(num_sites, num_filled_sites, hamiltonian);
-    std::cout << hamiltonian;
+    std::cout << hamiltonian << "\n\n";
+
+    const Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es(hamiltonian);
+    const Eigen::VectorXd ground_state = es.eigenvectors().col(0);
+    std::cout << ground_state << "\n\n";
+
+    Eigen::MatrixXd rdm;
+    fock::generate_rdm(num_sites, num_filled_sites, ground_state, {0, 1}, rdm);
+    std::cout << rdm;
 }
--- include/fock/library.hpp
+++ include/fock/library.hpp
@@ -52,14 +52,76 @@ namespace fock
         }
     }
 
+    inline std::uint64_t composite_state_bitmask(const std::uint64_t subregion_state_bitmask, const std::uint64_t complement_subregion_state_bitmask, const std::uint32_t num_sites, const std::vector<std::uint32_t>& subregion_indices)
+    {
+        const std::size_t subregion_size = subregion_indices.size();
+
+        std::uint64_t specified_mask = 0;
+        for (std::size_t i = 0; i < subregion_size; ++i) {
+            const std::uint32_t target_pos_from_right = num_sites - 1u - subregion_indices[i];
+            specified_mask |= 1ULL << target_pos_from_right;
+        }
+
+        std::uint64_t merged_result = 0;
+        for (std::size_t i = 0; i < subregion_size; ++i) {
+            const std::uint32_t target_pos_from_right = num_sites - 1u - subregion_indices[i];
+            if ((subregion_state_bitmask >> i & 1ULL) == 0) continue;
+            merged_result |= 1ULL << target_pos_from_right;
+        }
+
+        std::uint64_t current_complement_bits = complement_subregion_state_bitmask;
+        std::uint64_t gaps = ~specified_mask;
+        while (gaps) {
+            const std::uint64_t lsb = gaps & -gaps;
+            if ((current_complement_bits & 1ULL) != 0) merged_result |= lsb;
+            current_complement_bits >>= 1;
+            gaps ^= lsb;
+        }
+        return merged_result;
+    }
+
     // See https://stackoverflow.com/questions/21132538/correct-usage-of-the-eigenref-class for Eigen::Ref
-    inline void generate_rdm(std::uint32_t num_sites,
-                         std::uint32_t num_filled_sites,
+    inline void generate_rdm(const std::uint32_t num_sites,
+                         const std::uint32_t num_filled_sites,
                          const Eigen::Ref<const Eigen::VectorXd>&  eigenstate,
                          const std::vector<std::uint32_t>& subregion_indices,
                          Eigen::MatrixXd& rdm)
     {
-        // TODO: implementation
+        Eigen::MatrixXd density_matrix = eigenstate * eigenstate.transpose();
+
+        const std::size_t num_subregion_sites = subregion_indices.size();
+        const std::size_t num_complement_subregion_sites = num_sites - num_subregion_sites;
+
+        std::vector<std::uint64_t> subregion_states;
+        std::vector<std::uint64_t> complement_subregion_states;
+        generate_basis(num_subregion_sites, subregion_states);
+        generate_basis(num_complement_subregion_sites, complement_subregion_states);
+
+        std::vector<std::uint64_t> composite_states;
+        generate_basis(num_sites, num_filled_sites, composite_states);
+
+        rdm = Eigen::MatrixXd::Zero(1 << num_complement_subregion_sites, 1 << num_complement_subregion_sites);
+        for (std::size_t final_state_index = 0; final_state_index < 1 << num_subregion_sites; ++final_state_index)
+        {
+            for (std::size_t initial_state_index = 0; initial_state_index < 1 << num_subregion_sites; ++initial_state_index)
+            {
+                const auto x = subregion_states[final_state_index];
+                const auto y= subregion_states[initial_state_index];
+                if (__builtin_popcountll(x) != __builtin_popcountll(y)) continue; // if x and y dont have same count, then theres no way they form valid state
+                for (std::size_t j = 0; j < 1 << num_complement_subregion_sites; ++j)
+                {
+                    const auto z = complement_subregion_states[j];
+                    if (__builtin_popcountll(x) + __builtin_popcountll(z) != num_filled_sites || __builtin_popcountll(y) + __builtin_popcountll(z) != num_filled_sites) continue;
+                    const auto a = composite_state_bitmask(x, z,
+                            num_sites, subregion_indices);
+                    const auto b = composite_state_bitmask(y, z,
+                            num_sites, subregion_indices);
+                    const auto a_idx = std::distance(composite_states.begin(), std::ranges::find(composite_states, a));
+                    const auto b_idx = std::distance(composite_states.begin(), std::ranges::find(composite_states, b));
+                    rdm(final_state_index, initial_state_index) += density_matrix(a_idx, b_idx);
+                }
+            }
+        }
     }
 }
 
